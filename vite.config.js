226a64e632b9b0775eac2c0e0b4d583631135e4a
Saqib Hussain <dev.saqibhussain@gmail.com>
2023-08-02 17:07:45 +0500

    add: Script to package.json

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
+export default defineConfig({
     plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/react-vite-gh-pages/'
-  }
-
-  return config
+    base: '/amazon/',
 })
 
 
